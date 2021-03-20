81b6629b1616a99a56e6c1a9f60fb682814e36d2
Daniel Schauder <danschauder@gmail.com>
2021-03-20 19:31:48 -0400

    Experiment with visjs and dummy data

--- public/app.js
+++ public/app.js
@@ -1,10 +1,17 @@
-var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" }
-  ]);
+// var nodes = new vis.DataSet([
+//     { id: 1, label: "Node 1" },
+//     { id: 2, label: "Node 2" },
+//     { id: 3, label: "Node 3" },
+//     { id: 4, label: "Node 4" },
+//     { id: 5, label: "Node 5" }
+//   ]);
+
+const request = new XMLHttpRequest();
+request.open("GET","./data/nodes.json", false);
+request.send(null);
+const jsonData = JSON.parse(request.responseText);
+
+let nodes = new vis.DataSet(jsonData);
 
   // create an array with edges
   var edges = new vis.DataSet([
@@ -21,5 +28,11 @@ var nodes = new vis.DataSet([
     nodes: nodes,
     edges: edges
   };
-  var options = {};
+
+    const options = {
+        layout: {
+            improvedLayout: false
+        }
+    }
+
   var network = new vis.Network(container, data, options);
